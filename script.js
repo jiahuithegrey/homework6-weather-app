5fa9c64ddff37cc129d5da821f6b9e29199f0236
Jiahui Wang <wangjiahui@WangJiahuis-MacBook-Pro.local>
2019-10-25 09:48:34 -0700

    ux index

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var searchBtn = $(".fa-search");
+var lat;
+var lon;
 
-function SearchCityWeather(city){ //is city a property here?
+function SearchCityWeather(city){
     var APIKey = "04a910d3dcce5f3a689d56a30faedf19"; 
     var queryURL =
     "http://api.openweathermap.org/data/2.5/weather?"+"q="+city+ 
@@ -9,19 +11,36 @@ function SearchCityWeather(city){ //is city a property here?
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
-        $("#high-temperature").text("Highest Temperature: " + response.main.temp_max);
-        $("#low-temperature").text("Lowest Temperature: " + response.main.temp_min);
-        $("#humidity").text("Humidity: " + response.main.humidity);
-        $("#wind-speed").text("Wind Speed: " + response.wind.speed);
-        $("#uv-index").text(response.weather.main); //?????
+        var highTemperature = $("#high-temperature").text("Highest Temperature: " + response.main.temp_max);
+        var lowTemperature = $("#low-temperature").text("Lowest Temperature: " + response.main.temp_min);
+        var humidity = $("#humidity").text("Humidity: " + response.main.humidity);
+        var windSpeed = $("#wind-speed").text("Wind Speed: " + response.wind.speed);
+        lat = repsonse.coord.lat;
+        lon = response.coord.lon;
+    });
+    
+    var uvQueryURL =
+    "http://api.openweathermap.org/data/2.5/uvi?appid="+APIKey+"&lat="+lat+"&lon="+lon;
+    $.ajax({
+        url: uvQueryURL,
+        method: "GET"
+    }).then(function(response) {
+    var uvIndex = $("#uv-index").text(response.value); 
     });
 }
-searchBtn.on("click", function(event) {
-  event.preventDefault();
-  var cityEl = $(".city").val().trim(); //why value?
-  SearchCityWeather(cityEl);
+// function renderButtons() {
+
+// }
+$("#citySearch").on("submit", function(event) {
+    event.preventDefault();
+    var cityEl = $(".city").val().trim(); //otherwise var will be the whole input
+    SearchCityWeather(cityEl);
+
+    var searchHistory = $(".search-history");
+    searchHistory = cityEl.value;
 });
 
-
+searchBtn.on("click", function(event) { //how to make enter work too?
+    $("#citySearch").submit();
+});
 
