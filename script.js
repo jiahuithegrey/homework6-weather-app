35477cc2814238790c3730875dcd38bb9f665607
Jiahui Wang <wangjiahui@WangJiahuis-MacBook-Pro.local>
2019-10-26 15:50:36 -0700

    uv working finally

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-var searchBtn = $(".fa-search");
-var lat;
-var lon;
+// var searchBtn = $(".fa-search");
+var APIKey = "04a910d3dcce5f3a689d56a30faedf19"; 
 
-function SearchCityWeather(city){
-    var APIKey = "04a910d3dcce5f3a689d56a30faedf19"; 
+function getCityWeather(city){
     var queryURL =
     "http://api.openweathermap.org/data/2.5/weather?"+"q="+city+ 
     "&units=imperial&appid="+APIKey;
@@ -11,36 +9,49 @@ function SearchCityWeather(city){
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        var highTemperature = $("#high-temperature").text("Highest Temperature: " + response.main.temp_max);
-        var lowTemperature = $("#low-temperature").text("Lowest Temperature: " + response.main.temp_min);
-        var humidity = $("#humidity").text("Humidity: " + response.main.humidity);
-        var windSpeed = $("#wind-speed").text("Wind Speed: " + response.wind.speed);
-        lat = repsonse.coord.lat;
-        lon = response.coord.lon;
-    });
-    
-    var uvQueryURL =
-    "http://api.openweathermap.org/data/2.5/uvi?appid="+APIKey+"&lat="+lat+"&lon="+lon;
-    $.ajax({
-        url: uvQueryURL,
-        method: "GET"
-    }).then(function(response) {
-    var uvIndex = $("#uv-index").text(response.value); 
+        updateCityWeather(response);
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        getUVIndex(lat,lon);
     });
 }
+
+function updateCityWeather(response) {
+    $("#high-temperature").text("Highest Temperature: " + response.main.temp_max);
+    $("#low-temperature").text("Lowest Temperature: " + response.main.temp_min);
+    $("#humidity").text("Humidity: " + response.main.humidity);
+    $("#wind-speed").text("Wind Speed: " + response.wind.speed);
+}
+    
+function getUVIndex(lat,lon){
+    var QueryURL =
+    "http://api.openweathermap.org/data/2.5/uvi?appid="+APIKey+"&lat="+lat+"&lon="+lon;
+    $.ajax({
+        url: QueryURL,
+        method: "GET"
+    }).then(function(response) {
+        //console.log(response);
+        updateUVIndex(response);
+    });
+}
+
+function updateUVIndex(response) {
+    $("#uv-index").text(response.value); 
+}
+
 // function renderButtons() {
 
 // }
-$("#citySearch").on("submit", function(event) {
+$("#find-city").on("click", function(event) {
     event.preventDefault();
     var cityEl = $(".city").val().trim(); //otherwise var will be the whole input
-    SearchCityWeather(cityEl);
-
-    var searchHistory = $(".search-history");
-    searchHistory = cityEl.value;
+    getCityWeather(cityEl);
 });
 
-searchBtn.on("click", function(event) { //how to make enter work too?
-    $("#citySearch").submit();
-});
+//     var searchHistory = $(".search-history");
+//     searchHistory = cityEl.value;
+// });
 
+// searchBtn.on("click", function(event) { //how to make enter work too?
+//     $("#citySearch").submit();
+// });
